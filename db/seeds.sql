a1c90eeb50887f57c6feb362d482be546dcbdd31
Nicholas Jozefczyk <nickjay@Nicholass-MacBook-Air.local>
2018-08-24 19:34:03 -0700

    initial stuff

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Caliburger');
+INSERT INTO burgers (name) VALUES ('Messy Mary');
+INSERT INTO burgers (name, devour) VALUES ('Rodeo Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Big Mac', true);
+INSERT INTO burgers (name, devour) VALUES ('Whopper', true);
+INSERT INTO burgers (name) VALUES ('Impossible Burger');
